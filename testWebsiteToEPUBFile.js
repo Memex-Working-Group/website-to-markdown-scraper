59203c0b9aa020d20b89df5f9cd80b271d1be8d4
dentropy <dentropy@protonmail.com>
2026-04-19 18:25:51 -0400

    Let's just not do EPUB for now

--- testWebsiteToEPUBFile.js
+++
@@ -1,8 +0,0 @@
-import { getWebsiteAsEPUB } from './getWebsiteAsEPUB.js';
-
-let website = 'https://gwern.net/fiction/craneyard'
-let content = await getWebsiteAsEPUB(website)
-
-import fs from 'fs/promises';
-await fs.writeFile('./output/output.epub', content);
-console.log(content)
